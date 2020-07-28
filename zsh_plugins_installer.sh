b01b014525752a99f62fd83b7e48a0476bfaaf5f
Ansh Shukla <shuklaansh96@gmail.com>
2020-07-28 13:51:32 -0700

    zsh plugs

---
+++ zsh_plugins_installer.sh
@@ -0,0 +1,14 @@
+git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
+echo -e  "\nHey Cutie, pls add the following to the .zshrc:"
+echo \
+"plugins=(
+    git
+    zsh-nvm
+    zsh-autosuggestions
+    zsh-syntax-highlighting
+)"
